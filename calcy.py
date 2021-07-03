0d3cdd2b6032fe8df909ee46c28d963d232fce91
vigneshpusuluri <vignesh.pusuluri@gmail.com>
2021-07-03 13:53:32 +0530

    change in jenkins file

---
+++ calcy.py
@@ -0,0 +1,9 @@
+class Calcy:
+    #add 2 positive intigers
+    def sum(self, a, b):
+        return a + b
+    #subtraction on 2 intigers
+    def sub(self, a, b):
+        return a - b
+    def mul(self, a, b):
+        return a*b
